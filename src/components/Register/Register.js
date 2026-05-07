2bcf7d1ec5f5915d51f3105131e9fd9d99068e6e
Banoshree14499 <banoshreebose14499@gmail.com>
2026-05-07 16:29:55 +0530

    useform added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,25 +1,34 @@
-
+import useForm from "react-hook-form";
 
 const Register = () => {
-const onSubmit = (data) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
     console.log(data);
-    };
+  };
 
   return (
     <div>
       <h2>Register</h2>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" />
+          <input type="text" id="username" {...register("username", { required: true })} />
+          {errors.username && <p>{errors.username.message}</p>}
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" {...register("email", { required: true })} />
+          {errors.email && <p>{errors.email.message}</p>}
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" {...register("password", { required: true })} />
+          {errors.password && <p>{errors.password.message}</p>}
         </div>
         <button type="submit">Register</button>
       </form>
